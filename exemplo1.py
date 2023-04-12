2fff477b37103fb000fc4a26c7f3906918d18941
EC2 Default User <ec2-user@ip-172-31-88-82.ec2.internal>
2023-04-12 22:03:49 +0000

    Teste

---
+++ exemplo1.py
@@ -0,0 +1,11 @@
+dicionario = {"Gato":"Chat","cachorro":"Chien","Cavalo":"Cheval"}
+numeros = {"Chefe": 5551234567,"Julia":22657854310}
+vazio = {}
+
+print(dicionario)
+print(numeros)
+print(vazio)
+
+print(dicionario['Gato'])
+print(numeros['Julia'])
+
